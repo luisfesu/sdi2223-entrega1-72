f242d53e88c66f7576294741aaf2eb043a7366b7
luisfesu <luisfesu2000@gmail.com>
2023-03-11 15:14:02 +0100

    Commit Inicial

---
+++ src/main/java/com/uniovi/mywallapop/CustomConfiguration.java
@@ -0,0 +1,32 @@
+package com.uniovi.mywallapop;
+
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class CustomConfiguration implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("es", "ES"));
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
---
+++ src/main/java/com/uniovi/mywallapop/MywallapopApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.mywallapop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywallapopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MywallapopApplication.class, args);
+    }
+
+}
